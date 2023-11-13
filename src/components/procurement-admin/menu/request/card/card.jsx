747d4b357293655a6682c4ba16843cb67265062b
synectiks-fsd <synectiksfsd@gmail.com>
2023-11-13 14:21:00 +0530

    components

---
+++ src/components/procurement-admin/menu/request/card/card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./card.css";
+
+export const card = () => {
+  return (
+    <>
+      <div>card</div>
+      <div>card</div>
+      <div>card</div>
+
+      <div></div>
+    </>
+  );
+};
---
+++ src/components/procurement-admin/menu/request/tables/table.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./table.css";
+
+export const table = () => {
+  return (
+    <>
+      <div>table</div>
+      <div>table</div>
+      <div>table</div>
+    </>
+  );
+};
